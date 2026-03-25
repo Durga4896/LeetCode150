6b60c77948e4e542eb97e86189e9825002b75e14
DURGA SAI PRASAD YALAMKAYALA <durgasaiprasadyalamkayala@DURGAs-MacBook-Air.local>
2026-03-25 18:46:43 +0530

    Day3 LeetCode Done

---
+++ Arrays/O26_RemoveDuplicatesI.java
@@ -0,0 +1,15 @@
+public class O26_RemoveDuplicatesI {
+    public static void main(String[] args){
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i<nums.length-1 && nums[i] == nums[i+1]){
+                continue;
+            } else{
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
