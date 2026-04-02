3fd26840a784251e06d2e9aee2e72898377528f5
DURGA SAI PRASAD YALAMKAYALA <durasaiprasady@gmail.com>
2026-04-02 10:06:38 +0530

    Day11 Leetcode Done

---
+++ O28_IndexofFirstOccurance.java
@@ -0,0 +1,12 @@
+public class O28_IndexofFirstOccurance {
+    public static void main(String[] dspy){
+        String haystack = "DurgaSaiPrasad";
+        String needle = "sai";
+
+        if(haystack.contains(needle)){
+            System.out.println(haystack.indexOf(needle));
+        }else{
+            System.out.println(-1);
+        }
+    }
+}
