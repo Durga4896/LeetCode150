76fac93d7bfb1388d875f64e070e38c5c739b3a0
DURGA SAI PRASAD YALAMKAYALA <durgasaiprasady@gmail.com>
2026-04-10 22:32:43 +0530

    Day18 Leetcode Done

---
+++ Arrays/O35_SearchInsertPosition.java
@@ -0,0 +1,21 @@
+public class O35_SearchInsertPosition {
+    public static void main(String[] args){
+        int[] nums ={1,3,5,6};
+        int target = 7;
+
+        int start = 0;;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(nums[mid] == target){
+                start = mid;
+                break;
+            }else if(nums[mid] < target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        System.out.println(start);
+    }
+}
